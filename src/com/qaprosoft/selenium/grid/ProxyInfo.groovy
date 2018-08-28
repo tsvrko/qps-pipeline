af0dfadf164eb25ead5b229c0d2b59c56e27ea37
tsvrko <tsvrko@gmail.com>
2018-08-28 16:13:15 +0300

    Logger test for ProxyInfo

--- src/com/qaprosoft/selenium/grid/ProxyInfo.groovy
+++ src/com/qaprosoft/selenium/grid/ProxyInfo.groovy
@@ -4,12 +4,10 @@ import com.qaprosoft.Logger
 import com.qaprosoft.jenkins.jobdsl.factory.DslFactory
 import groovy.json.JsonSlurper
 
-class ProxyInfo {
+class ProxyInfo extends DslFactory{
 
     def private static platformDeviceListMap = ["android":[], "ios":[]]
     def private static baseDeviceList = ["DefaultPool", "ANY"]
-    def private static _dslFactory
-
 
 	//TODO: reused grid/admin/ProxyInfo to get atual list of iOS/Android devices
 	public static List<String> getDevicesList(String selenium, String platform) {
@@ -17,8 +15,7 @@ class ProxyInfo {
         //TODO: reuse selenium host/port/protocol from env jobVars
 		def proxyInfoUrl = selenium + "/grid/admin/ProxyInfo"
         def deviceList = platformDeviceListMap.get(platform.toLowerCase())
-        Logger.setOutput(_dslFactory)
-        Logger.log("Logger works!")
+        Logger.info("Logger works!")
 		try {
             if (deviceList.size() == 0) {
                 def json = new JsonSlurper().parse(proxyInfoUrl.toURL())
